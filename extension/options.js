893c57ce15f52bb3bfb97eb5fbe5a3b64bbab79f
Joe Jarvis <joe.jarvis109@gmail.com>
2017-10-29 13:41:54 -0400

    Started adjusting options to be cleaner

--- extension/options.js
+++ extension/options.js
@@ -1,23 +1,38 @@
-function deleteKey(ele) {
-    ele.addEventListener("click", () => {
-        browser.storage.local.remove(ele.className);
-        location.reload()
-    });
-}
-function changeKey(ele) {
-    ele.addEventListener("click", () => {
-        var newKey = prompt("Enter the new secret key for " + ele.className);
-        var obj = {};
-        obj[ele.className] = newKey;
-        browser.storage.local.set(obj);
-    });
-}
-function saveOptions(e) {
-    var obj = {};
-    obj[document.querySelector("#name").value] = document.querySelector("#num").value.replace(/\s/g, '');
-    browser.storage.local.set(obj);
-    e.preventDefault();
-    location.reload();
+const sites = document.getElementById("sites");
+// function deleteKey(ele) {
+//     ele.addEventListener("click", () => {
+//         browser.storage.local.remove(ele.className);
+//         location.reload()
+//     });
+// }
+// function changeKey(ele) {
+//     ele.addEventListener("click", () => {
+//         var newKey = prompt("Enter the new secret key for " + ele.className);
+//         var obj = {};
+//         obj[ele.className] = newKey;
+//         browser.storage.local.set(obj);
+//     });
+// }
+// function saveOptions(e) {
+//     var obj = {};
+//     obj[document.querySelector("#name").value] = document.querySelector("#num").value.replace(/\s/g, '');
+//     browser.storage.local.set(obj);
+//     e.preventDefault();
+//     location.reload();
+// }
+function createRow(info, target) {
+    var row = target.insertRow(-1);
+    for (var item of info) {
+        var cell = row.insertCell(-1);
+        var inner = document.createElement(item.element);
+        if (item.element === "input") {
+            inner.type = item.type;
+            inner.value = item.text;
+        } else {
+            inner.innerText = item.text;
+        }
+        cell.append(inner);
+    }
 }
 function exportSettings() {
     browser.storage.local.get().then((res) => {
@@ -35,30 +50,84 @@ function importSettings() {
     var file = document.getElementById("import").files[0];
     reader.readAsText(file);
 }
-function restoreOptions() {
+async function restoreOptions() {
     exportSettings();
-    browser.storage.local.get().then((res) => {
-        for (var i in res) {
-            if (res.hasOwnProperty(i)) {
-                var sites = document.getElementById("sites");
-                var name = document.createElement("span");
-                name.innerText = i;
-                var del = document.createElement("img");
-                del.src = "icons/delete.png";
-                del.className = i;
-                var edit = document.createElement("img");
-                edit.src = "icons/pencil.png";
-                edit.className = i;
-                sites.appendChild(del);
-                sites.appendChild(edit);
-                sites.appendChild(name);
-                sites.appendChild(document.createElement("br"));
-                deleteKey(del);
-                changeKey(edit);
+    var res = await browser.storage.local.get("otp_list")
+    for (var item of res.otp_list) {
+        createRow([
+            {
+                element: "input",
+                type: "text",
+                text: item.name,
+                listener: ["change", changeName]
+            },
+            {
+                element: "button",
+                text: "Change Key",
+                listener: ["click", openKeyInput]
+            },
+            {
+                element: "button",
+                text: "Delete",
+                listener: ["click", deleteSite]
             }
+        ], sites)
+    }
+    createRow([
+        {
+            element: "input",
+            type: "text",
+            text: ""
+        }, {
+            element: "input",
+            type: "text",
+            text: ""
+        }, {
+            element: "button",
+            text: "Add"
         }
-    });
+    ], sites);
+}
+function getRowIndex(element) {
+    return [...sites.children].indexOf(element.parentNode.parentNode) - 1;
+}
+async function changeName(e) {
+    var res = await browser.storage.local.get("otp_list");
+    var index = getRowIndex(e.target);
+    switch (index) {
+        case -1:
+        case sites.children.length - 2:
+            return;
+        default:
+            console.log(index);
+            res.otp_list[index].name = e.target.value;
+            browser.storage.local.set(res);
+    }
+}
+function openKeyInput(row) {
+    row.cells[1].innerText = "";
+    var input = document.createElement("input");
+    input.type = "text";
+    var change = document.createElement("button");
+    change.innerText = "Change";
+    row.cells[1].append(input);
+    row.cells[1].append(change);
+}
+function modifySite(e) {
+    switch (e.target.innerText) {
+        case "Change Key":
+            openKeyInput(e.target.parentNode.parentNode);
+            break;
+        case "Delete":
+            deleteItem(e.target.parentNode.parentNode);
+            break;
+        case "Add":
+            addItem(e.target.parentNode.parentNode);
+            break;
+    }
 }
 document.addEventListener("DOMContentLoaded", restoreOptions);
-document.getElementById("save").addEventListener("click", saveOptions);
+sites.addEventListener("change", changeName);
+sites.addEventListener("click", modifySite);
+// document.getElementById("save").addEventListener("click", saveOptions);
 document.getElementById("importButton").addEventListener("click", importSettings);
