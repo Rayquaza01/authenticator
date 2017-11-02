2ca6a5dd2fd8c14534de63cfda1240b34d4c1e6c
Joe Jarvis <joe.jarvis109@gmail.com>
2017-11-02 18:18:26 -0400

    Remade options UI to work with new storage format

--- extension/options.js
+++ extension/options.js
@@ -1,43 +1,21 @@
 const sites = document.getElementById("sites");
-// function deleteKey(ele) {
-//     ele.addEventListener("click", () => {
-//         browser.storage.local.remove(ele.className);
-//         location.reload()
-//     });
-// }
-// function changeKey(ele) {
-//     ele.addEventListener("click", () => {
-//         var newKey = prompt("Enter the new secret key for " + ele.className);
-//         var obj = {};
-//         obj[ele.className] = newKey;
-//         browser.storage.local.set(obj);
-//     });
-// }
-// function saveOptions(e) {
-//     var obj = {};
-//     obj[document.querySelector("#name").value] = document.querySelector("#num").value.replace(/\s/g, '');
-//     browser.storage.local.set(obj);
-//     e.preventDefault();
-//     location.reload();
-// }
-function createRow(info, target) {
-    var row = target.insertRow(-1);
-    for (var item of info) {
-        var cell = row.insertCell(-1);
-        var inner = document.createElement(item.element);
-        if (item.element === "input") {
-            inner.type = item.type;
-            inner.value = item.text;
-        } else {
-            inner.innerText = item.text;
-        }
-        cell.append(inner);
-    }
-}
-function exportSettings() {
-    browser.storage.local.get().then((res) => {
-        document.getElementById("export").href = "data:text/json;charset=utf-8," + JSON.stringify(res);
-    });
+const keyname = document.getElementById("keyname");
+const sitename = document.getElementById("sitename");
+const changeKey = document.getElementById("changeKey");
+const deleteSite = document.getElementById("deleteSite");
+const key = document.getElementById("key");
+const cancel = document.getElementById("cancel");
+const submitChange = document.getElementById("submitChange");
+const yes = document.getElementById("yes");
+const no = document.getElementById("no");
+const newName = document.getElementById("newName");
+const newKey = document.getElementById("newKey");
+const makeNew = document.getElementById("makeNew");
+const exportButton = document.getElementById("export");
+const importButton = document.getElementById("import");
+async function exportSettings() {
+    var res = await browser.storage.local.get();
+    exportButton.href = "data:text/json;charset=utf-8," + JSON.stringify(res);
 }
 function importSettings() {
     var reader = new FileReader();
@@ -47,12 +25,12 @@ function importSettings() {
         browser.storage.local.set(obj);
         location.reload();
     });
-    var file = document.getElementById("import").files[0];
+    var file = this.files[0];
     reader.readAsText(file);
 }
 async function restoreOptions() {
     exportSettings();
-    var res = await browser.storage.local.get("otp_list")
+    var res = await browser.storage.local.get("otp_list");
     for (var item of res.otp_list) {
         createRow([
             {
@@ -64,70 +42,94 @@ async function restoreOptions() {
             {
                 element: "button",
                 text: "Change Key",
-                listener: ["click", openKeyInput]
+                listener: ["click", modifySite]
             },
             {
                 element: "button",
                 text: "Delete",
-                listener: ["click", deleteSite]
+                listener: ["click", deleteKey]
             }
-        ], sites)
+        ], sites);
     }
+}
+function getRowIndex(element) {
+    return [...sites.children[0].children].indexOf(element.parentNode.parentNode) - 1;
+}
+async function changeName() {
+    var res = await browser.storage.local.get("otp_list");
+    var index = getRowIndex(this);
+    console.log(index);
+    if (index > -1) {
+        res.otp_list[index].name = this.value;
+        browser.storage.local.set(res);
+    }
+}
+async function modifySite() {
+    var res = await browser.storage.local.get("otp_list");
+    var index = getRowIndex(this);
+    keyname.innerText = res.otp_list[index].name;
+    submitChange.dataset.index = index;
+    key.focus();
+    changeKey.style.width = "100%";
+}
+async function deleteKey() {
+    var res = await browser.storage.local.get("otp_list");
+    var index = getRowIndex(this);
+    sitename.innerText = res.otp_list[index].name;
+    yes.dataset.index = index;
+    deleteSite.style.width = "100%";
+}
+function closeOverlays() {
+    changeKey.style.width = 0;
+    deleteSite.style.width = 0;
+}
+async function removeSite() {
+    var res = await browser.storage.local.get("otp_list");
+    res.otp_list.splice(this.dataset.index, 1)
+    browser.storage.local.set(res);
+    sites.deleteRow(parseInt(this.dataset.index) + 1)
+    closeOverlays();
+}
+async function submitKeyChange() {
+    var res = await browser.storage.local.get("otp_list");
+    res.otp_list[this.dataset.index].key = key.value.replace(/\s/g, "");
+    browser.storage.local.set(res);
+    key.value = "";
+    closeOverlays();
+}
+async function addSite() {
+    var res = await browser.storage.local.get("otp_list");
+    res.otp_list.push({
+        name: newName.value,
+        key: newKey.value.replace(/\s/g, "")
+    });
+    browser.storage.local.set(res);
     createRow([
         {
             element: "input",
             type: "text",
-            text: ""
-        }, {
-            element: "input",
-            type: "text",
-            text: ""
-        }, {
+            text: newName.value,
+            listener: ["change", changeName]
+        },
+        {
             element: "button",
-            text: "Add"
+            text: "Change Key",
+            listener: ["click", modifySite]
+        },
+        {
+            element: "button",
+            text: "Delete",
+            listener: ["click", deleteKey]
         }
     ], sites);
+    newName.value = "";
+    newKey.value = "";
 }
-function getRowIndex(element) {
-    return [...sites.children].indexOf(element.parentNode.parentNode) - 1;
-}
-async function changeName(e) {
-    var res = await browser.storage.local.get("otp_list");
-    var index = getRowIndex(e.target);
-    switch (index) {
-        case -1:
-        case sites.children.length - 2:
-            return;
-        default:
-            console.log(index);
-            res.otp_list[index].name = e.target.value;
-            browser.storage.local.set(res);
-    }
-}
-function openKeyInput(row) {
-    row.cells[1].innerText = "";
-    var input = document.createElement("input");
-    input.type = "text";
-    var change = document.createElement("button");
-    change.innerText = "Change";
-    row.cells[1].append(input);
-    row.cells[1].append(change);
-}
-function modifySite(e) {
-    switch (e.target.innerText) {
-        case "Change Key":
-            openKeyInput(e.target.parentNode.parentNode);
-            break;
-        case "Delete":
-            deleteItem(e.target.parentNode.parentNode);
-            break;
-        case "Add":
-            addItem(e.target.parentNode.parentNode);
-            break;
-    }
-}
+no.addEventListener("click", closeOverlays);
+yes.addEventListener("click", removeSite);
+submitChange.addEventListener("click", submitKeyChange);
+cancel.addEventListener("click", closeOverlays);
+makeNew.addEventListener("click", addSite);
+importButton.addEventListener("change", importSettings);
+browser.storage.onChanged.addListener(exportSettings);
 document.addEventListener("DOMContentLoaded", restoreOptions);
-sites.addEventListener("change", changeName);
-sites.addEventListener("click", modifySite);
-// document.getElementById("save").addEventListener("click", saveOptions);
-document.getElementById("importButton").addEventListener("click", importSettings);
