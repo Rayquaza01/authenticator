789e854b54bf56f9598cf8648965e50fd64ea6f4
Joe Jarvis <joe.jarvis109@gmail.com>
2017-11-02 18:19:46 -0400

    Use create row file to display the codes

--- extension/loader.js
+++ extension/loader.js
@@ -1,16 +1,14 @@
-function copyItem(ele) {
-    ele.addEventListener("click", () => {
-        var target = document.getElementById("hiddencopy");
-        target.textContent = ele.previousSibling.innerText;
-        target.focus();
-        target.setSelectionRange(0, target.value.length);
-        document.execCommand("copy");
-        target.blur();
-        ele.src = "icons/check.png";
-        setTimeout(() => {
-            ele.src = "icons/content-copy.png";
-        }, 1000);
-    });
+function copyTarget(ele) {
+    var target = document.getElementById("hiddencopy");
+    target.textContent = this.previousSibling.innerText;
+    target.focus();
+    target.setSelectionRange(0, target.value.length);
+    document.execCommand("copy");
+    target.blur();
+    this.src = "icons/check.svg";
+    setTimeout(() => {
+        this.src = "icons/content-copy.svg";
+    }, 1000);
 }
 function timeLoop() {
     var epoch = Math.round(new Date().getTime() / 1000.0);
@@ -19,35 +17,42 @@ function timeLoop() {
     if (epoch % 30 === 0) {
         var codes = document.getElementsByClassName("timecode");
         for (var code of codes) {
-            var timecode = otplib.authenticator.generate(code.dataset.key);
-            code.innerText = timecode;
+            code.innerText = otplib.authenticator.generate(code.dataset.key);
         }
     }
 }
 async function loadTOTP() {
     var res = await browser.storage.local.get("otp_list")
     for (var item of res.otp_list) {
-        var timecode = otplib.authenticator.generate(item.key);
-        var table = document.getElementById("totpbox");
-        var row = document.createElement("tr");
-        var name = document.createElement("td");
-        name.innerText = item.name;
-        name.className = "left";
-        var num = document.createElement("td");
-        num.className = "right";
-        var numText = document.createElement("span");
-        numText.innerText = timecode;
-        numText.className = "timecode right";
-        numText.dataset.key = item.key;
-        var copy = document.createElement("img");
-        copy.src = "icons/content-copy.png";
-        copy.className = "copy";
-        row.append(name);
-        num.append(numText);
-        num.append(copy);
-        row.append(num);
-        copyItem(copy);
-        table.append(row);
+        createRow([
+            [
+                {
+                    element: "parent",
+                    className: "left"
+                },
+                {
+                    element: "span",
+                    text: item.name
+                }
+            ], [
+                {
+                    element: "parent",
+                    className: "right"
+                }, {
+                    element: "span",
+                    text: otplib.authenticator.generate(item.key),
+                    className: "timecode",
+                    dataset: {
+                        key: item.key
+                    }
+                }, {
+                    element: "img",
+                    src: "icons/content-copy.svg",
+                    className: "copy",
+                    listener: ["click", copyTarget]
+                }
+            ]
+        ], document.getElementById("totpbox"));
     }
 }
 setInterval(timeLoop, 1000);
