ee976aead9ca99bbd43489ef5bc1b29220ebe417
Joe Jarvis <joe.jarvis109@gmail.com>
2017-11-08 19:00:27 -0500

    Size in popup set to 16px, click anywhere on row to copy

--- extension/loader.js
+++ extension/loader.js
@@ -1,60 +1,63 @@
-function copyTarget() {
-    var target = document.getElementById("hiddencopy");
-    target.textContent = this.previousSibling.innerText;
-    target.focus();
-    target.setSelectionRange(0, target.value.length);
+const totpbox = document.getElementById("totpbox");
+const hiddenCopy = document.getElementById("hiddencopy");
+const ticker = document.getElementById("ticker");
+
+function copyTarget(code, copy) {
+    console.log(code);
+    hiddenCopy.value = code.innerText;
+    hiddenCopy.focus();
+    hiddenCopy.setSelectionRange(0, hiddenCopy.value.length);
     document.execCommand("copy");
-    target.blur();
-    this.src = "icons/check.svg";
+    hiddenCopy.blur();
+    copy.src = "icons/check.svg";
     setTimeout(() => {
-        this.src = "icons/content-copy.svg";
+        copy.src = "icons/content-copy.svg";
     }, 1000);
 }
+
 function timeLoop() {
-    var epoch = Math.round(new Date().getTime() / 1000.0);
-    var countDown = 30 - (epoch % 30);
-    document.getElementById("ticker").innerText = countDown;
-    if (epoch % 30 === 0) {
+    var epoch = Math.floor(Date.now() / 1000);
+    var countDown = epoch % 30;
+    ticker.innerText = (30 - countDown);
+    if (countDown === 0) {
         var codes = document.getElementsByClassName("timecode");
         for (var code of codes) {
             code.innerText = otplib.authenticator.generate(code.dataset.key);
         }
     }
 }
+
+function createRow(item) {
+    var timecode =  otplib.authenticator.generate(item.key);
+
+    var row = totpbox.insertRow(-1);
+    var leftCol = row.insertCell(-1);
+    leftCol.className = "left";
+    var rightCol = row.insertCell(-1);
+    rightCol.className = "right";
+
+    var name = document.createElement("span");
+    name.innerText = item.name;
+    leftCol.append(name)
+
+    var code = document.createElement("span");
+    code.innerText = timecode;
+    code.className = "timecode right";
+    code.dataset.key = item.key;
+    rightCol.append(code);
+
+    var copy = document.createElement("img");
+    copy.src = "icons/content-copy.svg";
+    rightCol.append(copy);
+
+    row.addEventListener("click", copyTarget.bind(null, code, copy))
+}
+
 async function loadTOTP() {
     var res = await browser.storage.local.get("otp_list")
-    for (var item of res.otp_list) {
-        createRow([
-            [
-                {
-                    element: "parent",
-                    className: "left"
-                },
-                {
-                    element: "span",
-                    text: item.name
-                }
-            ], [
-                {
-                    element: "parent",
-                    className: "right"
-                }, {
-                    element: "span",
-                    text: otplib.authenticator.generate(item.key),
-                    className: "timecode",
-                    dataset: {
-                        key: item.key
-                    }
-                }, {
-                    element: "img",
-                    src: "icons/content-copy.svg",
-                    className: "copy",
-                    listener: ["click", copyTarget]
-                }
-            ]
-        ], document.getElementById("totpbox"));
-    }
+    res.otp_list.forEach(createRow);
 }
+
 setInterval(timeLoop, 1000);
 document.addEventListener("DOMContentLoaded", loadTOTP);
 document.getElementById("settings").addEventListener("click", () => {
