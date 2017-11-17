49bfb8a2b2c3d29426195c9df6db77b1fc449acc
Joe Jarvis <joe.jarvis109@gmail.com>
2017-11-17 16:40:43 -0500

    Fixed timer occasionally getting desynced, changed display format to use flex rather than a table

--- extension/loader.js
+++ extension/loader.js
@@ -3,12 +3,13 @@ const hiddenCopy = document.getElementById("hiddencopy");
 const ticker = document.getElementById("ticker");
 
 function copyTarget(code, copy) {
-    console.log(code);
+    // move code to textarea, copy
     hiddenCopy.value = code.innerText;
     hiddenCopy.focus();
     hiddenCopy.setSelectionRange(0, hiddenCopy.value.length);
     document.execCommand("copy");
     hiddenCopy.blur();
+    // Change icon from copy to check for 1s as visual feedback
     copy.src = "icons/check.svg";
     setTimeout(() => {
         copy.src = "icons/content-copy.svg";
@@ -16,50 +17,67 @@ function copyTarget(code, copy) {
 }
 
 function timeLoop() {
+    // from https://github.com/yeojz/otplib/blob/gh-pages/js/app.js#L65
     var epoch = Math.floor(Date.now() / 1000);
     var countDown = epoch % 30;
     ticker.innerText = (30 - countDown);
     if (countDown === 0) {
         var codes = document.getElementsByClassName("timecode");
         for (var code of codes) {
-            code.innerText = otplib.authenticator.generate(code.dataset.key);
+            if (code.dataset.key === "") {
+                continue;
+            } else {
+                code.innerText = otplib.authenticator.generate(code.dataset.key);
+            }
         }
     }
 }
 
 function createRow(item) {
-    var timecode =  otplib.authenticator.generate(item.key);
 
-    var row = totpbox.insertRow(-1);
-    var leftCol = row.insertCell(-1);
-    leftCol.className = "left";
-    var rightCol = row.insertCell(-1);
-    rightCol.className = "right";
+    var row = document.createElement("div");
+    row.className = "row";
+    totpbox.appendChild(row);
 
     var name = document.createElement("span");
     name.innerText = item.name;
-    leftCol.append(name)
+    name.className = "name";
+    row.appendChild(name);
 
     var code = document.createElement("span");
-    code.innerText = timecode;
-    code.className = "timecode right";
+    if (item.name === "") {
+        code.style.padding = 0;
+    }
+    if (item.key === "") {
+        code.innerText = "000000";
+        code.className = "timecode";
+    } else {
+        code.innerText = otplib.authenticator.generate(item.key);
+        code.className = "timecode";
+    }
     code.dataset.key = item.key;
-    rightCol.append(code);
+    row.appendChild(code);
 
     var copy = document.createElement("img");
     copy.src = "icons/content-copy.svg";
-    rightCol.append(copy);
+    copy.className = "copy";
+    row.appendChild(copy);
 
-    row.addEventListener("click", copyTarget.bind(null, code, copy))
+    row.addEventListener("click", copyTarget.bind(null, code, copy));
 }
 
 async function loadTOTP() {
     var res = await browser.storage.local.get("otp_list")
-    res.otp_list.forEach(createRow);
+    if (res.otp_list.length > 0) {
+        res.otp_list.forEach(createRow);
+    } else {
+        createRow({name: "No sites configured.", key: ""});
+    }
+    timeLoop(); // loads timer before waiting 1s
+    setInterval(timeLoop, 1000);
 }
 
-setInterval(timeLoop, 1000);
-document.addEventListener("DOMContentLoaded", loadTOTP);
 document.getElementById("settings").addEventListener("click", () => {
     browser.runtime.openOptionsPage();
 });
+document.addEventListener("DOMContentLoaded", loadTOTP);
