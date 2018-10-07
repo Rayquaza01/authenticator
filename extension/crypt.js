ac64ac37cfb168e22e1f68b9e1a728bf9f2608a2
Max1Truc <maxitruc.bonbec@gmail.com>
2018-10-07 14:46:40 +0200

    Updated crypt library with alphanumerical-only encryption.

--- extension/crypt.js
+++ extension/crypt.js
@@ -2,15 +2,21 @@
 // Source available on GitHub at https://GitHub.com/Max1Truc/crypt
 // It's under the MIT License
 
+function modulo(a, b) {
+  return a - (b * Math.floor(a / b))
+}
+
 function crypt(message, password) {
+  // ALPHABET, changing it changes characters which are encrypted
+  const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890 ";
   // initialize the crypted message var
   var crypted = '';
   // crypt each letter
   for (var i = 0; i < message.length; i++) {
     var indexLetterInText, indexLetterInPass, cryptedLetter;
-    indexLetterInText = message[i].charCodeAt(0);
-    indexLetterInPass = password[i % password.length].charCodeAt(0);
-    cryptedLetter = String.fromCharCode(indexLetterInText + indexLetterInPass);
+    indexLetterInText = modulo(ALPHABET.indexOf(message[i]), ALPHABET.length);
+    indexLetterInPass = modulo(ALPHABET.indexOf(password[modulo(i, password.length)]), ALPHABET.length);
+    cryptedLetter = ALPHABET[modulo(indexLetterInText + indexLetterInPass, ALPHABET.length)];
     crypted = crypted.concat(cryptedLetter);
   }
   // return
@@ -18,14 +24,16 @@ function crypt(message, password) {
 }
 
 function decrypt(cryptedMessage, password) {
+  // ALPHABET, changing it changes characters which are encrypted
+  const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890 ";
   // initialize the decrypted message var
   var decrypted = '';
   // decrypt each letter
   for (var i = 0; i < cryptedMessage.length; i++) {
     var indexLetterInText, indexLetterInPass, decryptedLetter;
-    indexLetterInText = cryptedMessage[i].charCodeAt(0);
-    indexLetterInPass = password[i % password.length].charCodeAt(0);
-    decryptedLetter = String.fromCharCode(indexLetterInText - indexLetterInPass);
+    indexLetterInText = modulo(ALPHABET.indexOf(cryptedMessage[i]), ALPHABET.length);
+    indexLetterInPass = modulo(ALPHABET.indexOf(password[modulo(i, password.length)]), ALPHABET.length);
+    decryptedLetter = ALPHABET[modulo(indexLetterInText - indexLetterInPass, ALPHABET.length)];
     decrypted = decrypted.concat(decryptedLetter);
   }
   // return
@@ -33,8 +41,8 @@ function decrypt(cryptedMessage, password) {
 }
 
 // Tests functions, else log it into the console
-var message = 'Hello World !';
+var message = 'Hello World';
 var pass = 'MyPassword';
 if (decrypt(crypt(message, pass), pass) !== message) {
-  console.error('Error in Max1Truc\'s Crypt Script !');
+  console.error('Error in Max1Truc\'s Crypt library !');
 }
