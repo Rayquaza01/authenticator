3538d570ad9f0776b5ae46ebd760af53af319b9a
Joe Jarvis <joe.jarvis109@gmail.com>
2021-01-31 11:03:46 -0500

    Clean, build html with js

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 
 module.exports = {
-    mode: "production",
+    mode: "development",
     entry: {
         background: __dirname + "/src/background.ts",
         popup: __dirname + "/src/ui/popup/index.ts",
         options: __dirname + "/src/ui/options/index.ts"
     },
-    devtool: "source-map",
+    devtool: "eval-cheap-source-map",
     output: {
         path: __dirname + "/dist",
         filename: "[name].bundle.js"
@@ -27,10 +29,31 @@ module.exports = {
             buffer: require.resolve("buffer/"),
             crypto: require.resolve("crypto-browserify"),
             stream: require.resolve("stream-browserify")
+        },
+        alias: {
+            process: "process/browser.js"
         }
     },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new webpack.ProvidePlugin({
+            process: "process/browser.js",
+            Buffer: ["buffer", "Buffer"]
+        }),
+        new HtmlWebpackPlugin({
+            template: "src/popup.html",
+            filename: "popup.html",
+            chunks: ["popup"],
+            cache: false
+        }),
+        new HtmlWebpackPlugin({
+            template: "src/options.html",
+            filename: "options.html",
+            chunks: ["options"],
+            cache: false
+        })
+    ],
     optimization: {
-        nodeEnv: "production",
         usedExports: true
     //     splitChunks: {
     //         chunks: "all"
