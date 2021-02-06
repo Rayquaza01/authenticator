926809b2a743fdb6490b752ea05fe47bef3ca0f9
Joe Jarvis <joe.jarvis109@gmail.com>
2021-02-06 15:42:58 -0500

    Load css using webpack

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ module.exports = {
                 test: /\.tsx?$/,
                 use: "ts-loader",
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
             }
         ]
     },
