b5c45c8100687f35dd0518a0c06f4c1ab3e84790
Joe Jarvis <joe.jarvis109@gmail.com>
2021-02-05 19:01:44 -0500

    Start read bits function

--- src/binary-encoding/BufReadWrite.ts
+++ src/binary-encoding/BufReadWrite.ts
@@ -6,11 +6,7 @@
  * @param count Number of bits to write from **data**, must be 8 or less
  */
 export function writeNBits(buf: Uint8Array, address: number, data: number, count: number): void {
-    if (count > 8 || count < 0) {
-        throw new RangeError("Can only write 8 or less bits at a time")
-    }
-
-    const byte = address >> 3;
+    const byte = address >> 0x3;
     const bit = address & 0x7;
 
     data = (data & (2 ** count - 1));
@@ -23,8 +19,22 @@ export function writeNBits(buf: Uint8Array, address: number, data: number, count
         buf[byte] |= data >> shiftBy;
         buf[byte + 1] |= data << (8 - shiftBy);
     }
+}
 
-    if (buf[byte] === 0) {
-        console.log(address, data, count)
+export function readNBits(buf: Readonly<Uint8Array>, address: number, count: number): number {
+    let result = 0;
+
+    const byte = address >> 0x3;
+    const bit = address & 0x7;
+
+    let shiftBy = Math.abs(bit - (8 - count));
+
+    if (bit <= 8 - count) {
+        result = buf[byte] >> shiftBy;
+    } else {
+        result = buf[byte] << shiftBy;
+        result |= buf[byte + 1] >> (8 - shiftBy);
     }
+
+    return result;
 }
