22ce96f1de4685e588b5cadc4d6a7552568faed0
Joe Jarvis <joe.jarvis109@gmail.com>
2021-02-06 18:41:06 -0500

    Add old encrypt script to src

---
+++ src/old/crypt.d.ts
@@ -0,0 +1,2 @@
+export function crypt(message: string, password: string): string;
+export function decrypt(cryptedMessage: string, password: string): string;
--- src/old/crypt.js
+++ src/old/crypt.js
@@ -1,3 +1,8 @@
+// This file is not used for encryption in authenticatior anymore.
+// It is still used for upgrading from pre-2.0.0 versions
+
+
+
 // Script created by Max1Truc
 // Source available on GitHub at https://GitHub.com/Max1Truc/crypt
 // It's under the MIT License
@@ -6,7 +11,7 @@ function modulo(a, b) {
   return a - (b * Math.floor(a / b));
 }
 
-function crypt(message, password) {
+export function crypt(message, password) {
   // ALPHABET, changing it changes characters which are encrypted
   const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 =";
   // initialize the crypted message var
@@ -23,7 +28,7 @@ function crypt(message, password) {
   return crypted;
 }
 
-function decrypt(cryptedMessage, password) {
+export function decrypt(cryptedMessage, password) {
   // ALPHABET, changing it changes characters which are encrypted
   const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 =";
   // initialize the decrypted message var
@@ -38,11 +43,4 @@ function decrypt(cryptedMessage, password) {
   }
   // return
   return decrypted;
-}
-
-// Tests functions, else log it into the console
-var message = 'Hello World, this letter does not work with "2" as a password : I';
-var pass = 'Password 2';
-if (decrypt(crypt(message, pass), pass) !== message) {
-  console.error('Error in Max1Truc\'s Crypt library !');
 }
