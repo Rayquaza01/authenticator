505a78878f38245953e4d58522a4422969e42a17
Joe Jarvis <joe.jarvis109@gmail.com>
2021-02-06 15:41:52 -0500

    Change base32 usage in totp

--- src/OTP/TOTP.ts
+++ src/OTP/TOTP.ts
@@ -1,12 +1,12 @@
 import { HOTP } from "./HOTP";
-import { Base32 } from "../binary-encoding/Base32";
+import { decode } from "../base-encoding/BaseEncoding";
 
 const INTERVAL = 30;
 
 export async function TOTP(key: string): Promise<string> {
     // must base 32 decode key to be compatible with google authenticator
 
-    const decodedKey = Base32.decode(key);
+    const decodedKey = decode(key, "base32");
 
     const counter = Math.floor(Math.floor(Date.now() / 1000) / INTERVAL);
 
